4afe4f087483898e5f49c7e48543a98195e1f150
rizki <rizki_syarif@brown.edu>
2016-05-25 10:43:08 -0500

    naive step1 error check

---
+++ Step1ErrorCheck.sh
@@ -0,0 +1,4 @@
+echo 'ls -l /uscms_data/d3/rsyarif/Spring2016/TprimeAnalysis_BrownFrameWork/LJMet_3lep_122115_step1_v2/*/*/*.job | wc'
+ls -l /uscms_data/d3/rsyarif/Spring2016/TprimeAnalysis_BrownFrameWork/LJMet_3lep_122115_step1_v2/*/*/*.job | wc
+echo 'grep "Npassed_ALL" /uscms_data/d3/rsyarif/Spring2016/TprimeAnalysis_BrownFrameWork/LJMet_3lep_122115_step1_v2/*/*/*.out | wc'
+grep "Npassed_ALL" /uscms_data/d3/rsyarif/Spring2016/TprimeAnalysis_BrownFrameWork/LJMet_3lep_122115_step1_v2/*/*/*.out | wc
